d399bde40db2ae8d97faa7044c7bcb5be5b29ff7
Aresh N <126131176+AreshNatarajan@users.noreply.github.com>
2026-08-12 19:06:08 +0530

    GLOABL URI

--- src/server.js
+++ src/server.js
@@ -25,8 +25,8 @@ app.set('trust proxy', 1);
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://shopping-frontend-c82v.onrender.com",
-    // "https://allways-apple-products-frontend.onrender.com"
+    // "https://shopping-frontend-c82v.onrender.com",
+    "https://allways-apple-products-frontend.onrender.com"
 ];
 
 app.use(cors({
